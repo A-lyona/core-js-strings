b990a9547e3ca5d6ad31bbd74172f1545c33b2de
Alena Labuko <79700929+A-lyona@users.noreply.github.com>
2025-09-22 16:12:01 +0300

    feat: add string

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -136,8 +136,11 @@ return value.trimEnd();
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (times > 0) {
+    return str.repeat(times);
+  }
+  return '';
 }
 
 /**
